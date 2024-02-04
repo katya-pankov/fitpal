b8dcdaa2b8ba9211a86934d35960c3afd465a626
katya-pankov <e.pankov@students.clark.edu>
2024-02-04 14:48:26 -0800

    Added remove meals and wrorkouts from localStorage

--- js/app.js
+++ js/app.js
@@ -40,6 +40,7 @@ class CalorieTracker {
             this._totalCalories -= meal.calories;
             Storage.updateTotalCalories(this._totalCalories);
             this._meals.splice(index, 1);
+            Storage.removeMeal(id);
             this._render();
         }
     }
@@ -52,6 +53,7 @@ class CalorieTracker {
             Storage.updateTotalCalories(this._totalCalories);
             this._totalCalories += workout.calories;
             this._workouts.splice(index, 1);
+            Storage.removeWorkout(id);
             this._render();
         }
     }
@@ -250,6 +252,17 @@ class Storage {
         localStorage.setItem('meals', JSON.stringify(meals));
     }
 
+    static removeMeal(id) {
+        const meals = Storage.getMeals();
+        meals.forEach((meal, index) => {
+            if (meal.id === id) {
+                meals.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('meals', JSON.stringify(meals));
+    }
+
     static getWorkouts() {
         let workouts;
         if (localStorage.getItem('workouts') === null) {
@@ -265,6 +278,17 @@ class Storage {
         const workouts = Storage.getWorkouts();
         workouts.push(workout);
         localStorage.setItem('workouts', JSON.stringify(workouts));
+    }
+
+    static removeWorkout(id) {
+        const workouts = Storage.getWorkouts();
+        workouts.forEach((workout, index) => {
+            if (workout.id === id) {
+                workouts.splice(index, 1);
+            }
+        });
+
+        loaclStoarge.setItem('meals', JSON.stringify(workouts));
     }
 
 }
